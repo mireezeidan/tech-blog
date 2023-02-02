9249c4bf6c1329a75ae748d7ebc717d0386d609f
Miree Zeidan <mireezeidan@gmail.com>
2023-02-01 23:09:35 -0600

    added login and logout feature, as well as user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 
-const postRoutes = require('./post-routes');
+const postRoutes = require("./post-routes");
+const userRoutes = require("./user-routes");
 
-router.use('/post', postRoutes);
+router.use("/post", postRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,13 +1,24 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const withAuth = require("../utils/auth");
 
-// route to get all dishes
+// route to get all posts
 router.get("/", async (req, res) => {
-    const postData = await Post.findAll().catch((err) => {
-      res.json(err);
-    });
-    const posts = postData.map((post) => post.get({ plain: true }));
-  res.render("all", { posts });
+  const postData = await Post.findAll().catch((err) => {
+    res.json(err);
+  });
+  const posts = postData.map((post) => post.get({ plain: true }));
+  res.render("all", { posts, loggedIn: req.session.loggedIn });
+});
+
+// route to login page
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
 });
 
 module.exports = router;
